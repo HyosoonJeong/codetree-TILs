42d112c34c6843ab0a3d66dd981cbd9d17c67669
HyosoonJeong <165642736+HyosoonJeong@users.noreply.github.com>
2024-04-08 15:00:15 +0900

    Title: 격자 반대로 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240408/격자 반대로 채우기/grid-anti-fill.cpp
+++ 240408/격자 반대로 채우기/grid-anti-fill.cpp
@@ -5,36 +5,29 @@ int main() {
     int n;
     cin >> n;
 
-    int cnt = 0;
+    int cnt = 1;
     int arr[10][10];
 
     // 입력
-    for (int j = n-1; j >= 0; j--)
-    {
-        if (j % 2 == 0)
-        {
-            for (int i = n-1; i >= 0; i--)
-            {
-                arr[n-(n-i)][j] = ++cnt;
+    for (int j = n - 1; j >= 0; j--) {
+        if ((n - j) % 2 == 1) { // 위에서 아래로
+            for (int i = n - 1; i >= 0; i--) {
+                arr[i][j] = cnt++;
             }
         }
-        else
-        {
-            for (int i = 0; i < n; i++)
-            {
-                arr[i][j] = ++cnt;
+        else { // 아래에서 위로
+            for (int i = 0; i < n; i++) {
+                arr[i][j] = cnt++;
             }
         }
     }
 
     // 출력
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++)
             cout << arr[i][j] << " ";
         cout << "\n";
     }
-
 
     return 0;
 }
