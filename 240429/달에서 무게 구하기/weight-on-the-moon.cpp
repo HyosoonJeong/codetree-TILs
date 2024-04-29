74f26258e2f53024e2c66a02e1dab6e330907a8f
HyosoonJeong <165642736+HyosoonJeong@users.noreply.github.com>
2024-04-29 20:16:52 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240429/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int wei=13;
+    double grav=0.165;
+cout<<fixed;
+    cout.precision(6);
+    cout<<wei<<" * "<<grav<< " = ";
+    
+    cout<<wei*grav;
+
+    return 0;
+}
