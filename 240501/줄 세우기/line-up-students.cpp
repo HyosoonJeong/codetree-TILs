288b8404f9005e7a0c58fb210c8d5be9f56e68e1
HyosoonJeong <165642736+HyosoonJeong@users.noreply.github.com>
2024-05-01 22:06:45 +0900

    Title: 줄 세우기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240501/줄 세우기/line-up-students.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+#include <algorithm>
+
+class Student {
+    public:
+    int height;
+    int weight;
+    int number;
+    Student (int height, int weight, int number) {
+        this->height=height;
+        this->weight=weight;
+        this->number=number;
+    }
+    Student () {}
+};
+
+bool cmp(Student a, Student b) {
+    if(a.height>b.height)
+        return a.height>b.height;
+    else if(a.height==b.height)
+        return a.weight>b.weight;
+    else 
+        return a.number<b.number;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    Student student = Student();
+    Student arr[10];
+    for(int i=0; i<n; i++) {
+        cin>>arr[i].height>>arr[i].weight;
+        arr[i].number=i+1;
+    }
+    sort(arr, arr+n, cmp);
+    for(int i=0; i<n; i++) {
+        cout<<arr[i].height<<" "<<arr[i].weight<<" "<<arr[i].number<<"\n";
+    }
+
+    return 0;
+}
