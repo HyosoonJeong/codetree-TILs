3eb36f88b9c6761ae314494df974f5c88049f7b7
HyosoonJeong <165642736+HyosoonJeong@users.noreply.github.com>
2024-05-07 15:14:12 +0900

    Title: Time to Time, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240507/Time to Time/time-to-time.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin>>a>>b>>c>>d;
+
+    int elapsed_time=0;
+
+    while(true) {
+        if(a==c && b==d)
+            break;
+        elapsed_time++;
+        b++;
+
+        if(b==60) {
+            a++;
+            b=0;
+        }
+    }
+    cout<<elapsed_time;
+    return 0;
+}
