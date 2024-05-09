7bc6ff6565dba8a5c36b1c2df23c5c9cfb586606
HyosoonJeong <165642736+HyosoonJeong@users.noreply.github.com>
2024-05-09 20:23:55 +0900

    Title: 최고의 13위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240509/최고의 13위치/best-place-of-13.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin>>N;
+
+    int arr[20][20]={-1};
+
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<N; j++) {
+            cin>>arr[i][j];
+        }
+    }
+    int max=0;
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<N; j++) {
+            if(max<arr[i][j]+arr[i][j+1]+arr[i][j+2])
+                max=arr[i][j]+arr[i][j+1]+arr[i][j+2];
+        }
+    }
+    cout<<max;
+    return 0;
+}
