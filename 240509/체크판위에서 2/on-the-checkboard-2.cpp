19a93e3a198412e55da3999c56310f244097da76
HyosoonJeong <165642736+HyosoonJeong@users.noreply.github.com>
2024-05-09 21:39:03 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240509/체크판위에서 2/on-the-checkboard-2.cpp
+++ 240509/체크판위에서 2/on-the-checkboard-2.cpp
@@ -14,8 +14,8 @@ int main() {
 
     int cnt=0;
 
-    for(int i=0; i<R; i++) {
-        for(int j=0; j<C; j++) {
+    for(int i=1; i<R; i++) {
+        for(int j=1; j<C; j++) {
             for(int k=i+1; k<R-1; k++) {
                 for(int l=j+1; l<C-1; l++) {
                     if(arr[0][0]!=arr[i][j] && arr[i][j]!=arr[k][l] && arr[k][l]!=arr[R-1][C-1])
